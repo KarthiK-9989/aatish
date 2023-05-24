ca41139b86a9bb636a9100accf09c41878ddb3cb
Karthik Kondpak <karthikkondpak44@gmail.com>
2023-05-24 13:03:06 +0530

    Tuple related changes

---
+++ src/main/scala/tupledemo.scala
@@ -0,0 +1,18 @@
+object tupledemo {
+  def main(args:Array[String]): Unit={
+
+    val x=Set("karthik","karthik","karthik","vijay","vijay","deepak","shivani")
+
+    println(x);
+
+    //1,2,3,4,5,6,7
+
+
+    //set always holds unique values
+
+    //order is never maintained
+
+
+  }
+
+}
